fc5af58b7ec78ed4d18101e6b726ffc3f5cfb600
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-26 01:19:43 +0530

    arth-math package comments add

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 
-export function addTwo(first,second) {
+export function addTwo(first,second) { // for addtion
     const res =first+second;
     return res;
 }
 
-export function subTwo(first,second) {
+export function subTwo(first,second) { // for substraction
     const res = first-second;
     return res;
 }
-export function mulTwo(first,second) {
+export function mulTwo(first,second) { // multiplication
     const res = first*second;
     return res;
 }
-export function divTwo(first,second) {
+export function divTwo(first,second) { // devide
     const res = first/second;
     return res;
 }
-export function remTwo(first,second) {
+export function remTwo(first,second) {  // for remander
     const res = first%second;
     return res;
 }
