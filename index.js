5dd721ab9b6d4997a664e004971941883c56f1ce
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-26 01:00:37 +0530

    arth-math package

---
+++ index.js
@@ -0,0 +1,22 @@
+
+export function addTwo(first,second) {
+    const res =first+second;
+    return res;
+}
+
+export function subTwo(first,second) {
+    const res = first-second;
+    return res;
+}
+export function mulTwo(first,second) {
+    const res = first*second;
+    return res;
+}
+export function divTwo(first,second) {
+    const res = first/second;
+    return res;
+}
+export function remTwo(first,second) {
+    const res = first%second;
+    return res;
+}
